8ca15ea04ccab8634fefb78c4574f30971902fa7
Lucas Guerrero <lucas.1807@hotmail.com>
2024-06-10 14:08:36 -0300

    feat: addind outputs eks module

--- eks/output.tf
+++ eks/output.tf
@@ -5,3 +5,13 @@ output "eks_cluster" {
 output "security_group" {
   value = aws_security_group.cluster_master_sg
 }
+
+output "endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "certificate_authority_data" {
+  description = "The certificate authority data for the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}

7ba308fe7633ff892395811c00e0fd9d3cd14edf
Lucas Guerrero <lucas.1807@hotmail.com>
2024-06-10 11:21:35 -0300

    fix: fixing eks and nodes modulo to use the new env

--- nodes/node_group.tf
+++ nodes/node_group.tf
@@ -4,10 +4,7 @@ resource "aws_eks_node_group" "cluster" {
     node_group_name = format("%s-node-group", var.cluster_name)
     node_role_arn = aws_iam_role.eks_nodes_roles.arn
 
-    subnet_ids = [
-        var.private_subnet_1a_id,
-        var.private_subnet_1c_id
-    ]
+    subnet_ids = var.private_subnet_ids
 
     instance_types = var.nodes_instances_types
 

5d6b11ee0d85ae5c4dcf8018ba972652150b00ad
Lucas Guerrero <lucas.1807@hotmail.com>
2024-06-12 11:22:59 -0300

    fix: final fix for grafana and prometheus

--- eks/grafana/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "helm" {
-  kubernetes {
-    host                   = var.endpoint
-    cluster_ca_certificate = base64decode(var.certificate_authority_data)
-    token                  = var.token
-  }
-}
--- eks/prometheus/prometheus.tf
+++ eks/prometheus/prometheus.tf
@@ -2,41 +2,6 @@ resource "aws_ebs_volume" "prometheus" {
   availability_zone = "us-east-1a"
   size              = 10
 }
-/* "prometheus" {
-  metadata {
-    name = "prometheus-pv"
-  }
-  spec {
-    capacity = {
-      storage = "10Gi"
-    }
-    access_modes = ["ReadWriteOnce"]
-    persistent_volume_reclaim_policy = "Retain"
-    storage_class_name = "standard"
-    aws_elastic_block_store {
-      volume_id = aws_ebs_volume.prometheus.id
-      fs_type   = "ext4"
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "prometheus" {
-  metadata {
-    name      = "prometheus-pvc"
-    namespace = "monitoring"
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = "10Gi"
-      }
-    }
-    volume_name = kubernetes_persistent_volume.prometheus.metadata[0].name
-    storage_class_name = "standard"
-  }
-}
-*/
 
 resource "helm_release" "prometheus" {
   name             = "prometheus"

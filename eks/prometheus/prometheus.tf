4b9be6d09245ccf375a9ee33c662e5f1f4319240
Lucas Guerrero <lucas.1807@hotmail.com>
2024-06-12 10:16:47 -0300

    fix: fixing prometheus deploy

--- eks/prometheus/prometheus.tf
+++ eks/prometheus/prometheus.tf
@@ -1,3 +1,25 @@
+resource "aws_ebs_volume" "prometheus" {
+  availability_zone = "us-east-1a"
+  size              = 10
+}
+/* "prometheus" {
+  metadata {
+    name = "prometheus-pv"
+  }
+  spec {
+    capacity = {
+      storage = "10Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name = "standard"
+    aws_elastic_block_store {
+      volume_id = aws_ebs_volume.prometheus.id
+      fs_type   = "ext4"
+    }
+  }
+}
+
 resource "kubernetes_persistent_volume_claim" "prometheus" {
   metadata {
     name      = "prometheus-pvc"
@@ -10,18 +32,32 @@ resource "kubernetes_persistent_volume_claim" "prometheus" {
         storage = "10Gi"
       }
     }
+    volume_name = kubernetes_persistent_volume.prometheus.metadata[0].name
+    storage_class_name = "standard"
   }
 }
+*/
 
 resource "helm_release" "prometheus" {
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
+  chart            = "kube-prometheus-stack"
   namespace        = "monitoring"
+  version          = "45.7.1"
   create_namespace = true
 
   set {
-    name  = "server.persistentVolume.existingClaim"
-    value = kubernetes_persistent_volume_claim.prometheus.metadata[0].name
+    name  = "server.persistentVolume.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "alertmanager.persistentVolume.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "pushgateway.persistentVolume.enabled"
+    value = "false"
   }
 }
